77aeae78448d96c7da65c444311b5c84e4e5a6de
Jonathan Sotelo <jonathan.sotelo@outlook.com>
2017-09-20 00:23:24 -0600

    Testing

--- ImageGenetic.Console/Program.cs
+++ ImageGenetic.Console/Program.cs
@@ -6,7 +6,7 @@ namespace ImageGenetic.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
